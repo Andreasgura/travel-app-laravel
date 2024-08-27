3ce33b0bfff9971c4c6d3d6f37d061937097175e
Andrea Sgura <andre-56@hotmail.it>
2024-08-27 16:27:47 +0200

    set up and db migrations

---
+++ database/migrations/2024_08_27_125547_create_days_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->id();
+            $table->string('day_number');
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('travel_id');
+            $table->foreign('travel_id')->references('id')->on('travels')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('days');
+    }
+};
---
+++ database/migrations/2024_08_27_125731_create_stages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->unsignedInteger('rate')->nullable();
+            $table->string('note')->nullable();
+            $table->decimal('lat',30,15)->nullable();
+            $table->decimal('long',30,15)->nullable();
+            $table->unsignedBigInteger('day_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stages');
+    }
+};
